b2ae4bf11d41bf51c2c1f84d72a02df33d25030e
DENIS PATRÃO MACHADO JÚNIOR <denispatrao@yahoo.com.br>
2025-03-05 15:53:17 -0300

    inserção de dados na conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Qual o número da conta: ");
+        int numeroConta = scan.nextInt();
+
+        System.out.println("Qual o número da Agência: ");
+        String agencia = scan.next();
+
+        System.out.println("Nome do Titular da conta");
+        String nome = scan.next();
+
+        System.out.println("Sobrenome do Titular da conta");
+        String sobreNome = scan.next();
+        
+        System.out.println("Entre com o saldo: ");
+        double saldo = scan.nextDouble();
+
+        System.out.println("Olá, " + nome + " " + sobreNome +
+        ", obrigado por criar uma conta em nosso banco, sua agencia é: " + 
+        agencia + ", conta: " + numeroConta + " e seu saldo R$ " + saldo + 
+        " já está disponível para saque!");
+
     }
 }
